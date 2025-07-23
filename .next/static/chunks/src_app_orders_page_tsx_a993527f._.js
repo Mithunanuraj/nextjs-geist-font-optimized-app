9305d6ad4f46dff53c93ec61f72520529af0ae5a
Mithunanuraj <147816781+Mithunanuraj@users.noreply.github.com>
2025-07-23 08:31:16 +0530

    Add .next/static/chunks/src_app_orders_page_tsx_a993527f._.js 1753239677083

---
+++ .next/static/chunks/src_app_orders_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_a477839c._.js"
+],
+    source: "dynamic"
+});
